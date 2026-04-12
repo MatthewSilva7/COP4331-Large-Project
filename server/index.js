02da090270cd3a14da04c6458924c2f6744edab3
kajal0223 <sachi2030@yahoo.com>
2026-04-12 16:35:06 -0400

    Revert "Merge pull request #3 from MatthewSilva7/view-profile"
    This reverts commit 9f6cfae60a49cb077e63c4fe5163ab188d71dc9b, reversing
    changes made to c2fc59755a62f74e6dde0f0ef02af41cbf6fc8b3.

--- server/index.js
+++ server/index.js
@@ -7,7 +7,6 @@ require('dotenv').config();
 
 const authRoutes = require('./routes/auth');
 const sessionRoutes = require('./routes/sessions');
-const userRoutes = require('./routes/user');
 
 const app = express();
 
@@ -18,7 +17,6 @@ app.use(express.json()); // Allows us to read JSON from the frontend
 // Routes
 app.use('/api/auth', authRoutes);
 app.use('/api/sessions', sessionRoutes);
-app.use('/api/user', userRoutes);
 
 // Database Connection
 mongoose.connect(process.env.MONGO_URI)
--- server/models/User.js
+++ server/models/User.js
@@ -1,11 +1,5 @@
 const mongoose = require('mongoose');
 
-// Define course structure
-const CourseSchema = new mongoose.Schema({
-    subject: { type: String, uppercase: true }, // e.g., "COP"
-    number: { type: String }                    // e.g., "4331"
-});
-
 const UserSchema = new mongoose.Schema({
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
@@ -13,8 +7,6 @@ const UserSchema = new mongoose.Schema({
   password: { type: String, required: true },
   emailVerified: { type: Boolean, default: false }, // For the 5pt rubric requirement
   verificationToken: { type: String } // To be used for the email link
-  major: { type: String, default: "" },
-  courses: [CourseSchema] // Creates an array of course objects
 });
 
 module.exports = mongoose.model('users', UserSchema);
